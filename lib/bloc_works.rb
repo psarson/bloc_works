e45881ea6fdd5358a39572a1689c276dee37240c
Patrick Sarson <sarsonmedia@gmail.com>
2017-02-20 22:59:09 -0500

    add checkpoint-2

--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -2,24 +2,19 @@ require "bloc_works/version"
 require "bloc_works/router"
 require "bloc_works/utility"
 require "bloc_works/dependencies"
+require "bloc_works/controller"
 
 module BlocWorks
   class Application
      def call(env)
-       if env['PATH_INFO'] == '/favicon.ico'
-         return [404, {'Content-Type' => 'text/html'}, []]
-       end
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
 
-       klass, action = controller_and_action(env)
-       controller = klass.new(env)
-       text = controller.send(action)
-       [200, {'Content-Type' => 'text/html'}, [text]]
-     end
-   end
-
-   class Controller
-     def initialize(env)
-        @env = env
-     end
-   end 
+      klass, action = controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(action)
+      [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+  end
 end
--- lib/bloc_works/router.rb
+++ lib/bloc_works/router.rb
@@ -1,16 +1,18 @@
-class Application
-  def controller_and_action(env)
-     _, controller, action, _ = env["PATH_INFO"].split("/", 4)
-     controller = controller.capitalize
-     controller = "#{controller}Controller"
+module BlocWorks
+  class Application
+    def controller_and_action(env)
+       _, controller, action, _ = env["PATH_INFO"].split("/", 4)
+       controller = controller.capitalize
+       controller = "#{controller}Controller"
 
-     [Object.const_get(controller), action]
-   end
+       [Object.const_get(controller), action]
+     end
 
-   def fav_icon(env)
-     puts env['PATH_INFO']
-     if env['PATH_INFO'] == '/favicon.ico'
-       return [404, {'Content-Type' => 'text/html'}, []]
+     def fav_icon(env)
+       puts env['PATH_INFO']
+       if env['PATH_INFO'] == '/favicon.ico'
+         return [404, {'Content-Type' => 'text/html'}, []]
+       end
      end
    end
 end
