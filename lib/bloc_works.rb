6451075da91ea5bdb86bae610f3eb88017383a67
Patrick Sarson <sarsonmedia@gmail.com>
2017-02-20 20:57:04 -0500

    add checkpoint-1

--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -1,9 +1,25 @@
 require "bloc_works/version"
+require "bloc_works/router"
+require "bloc_works/utility"
+require "bloc_works/dependencies"
 
 module BlocWorks
   class Application
      def call(env)
-       [200, {'Content-Type' => 'text/html'}, ["Hello Blocheads!"]]
+       if env['PATH_INFO'] == '/favicon.ico'
+         return [404, {'Content-Type' => 'text/html'}, []]
+       end
+
+       klass, action = controller_and_action(env)
+       controller = klass.new(env)
+       text = controller.send(action)
+       [200, {'Content-Type' => 'text/html'}, [text]]
      end
    end
+
+   class Controller
+     def initialize(env)
+        @env = env
+     end
+   end 
 end
