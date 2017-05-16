b51a5823cd11c0b94c8a479f8e89f5728595a35d
Patrick Sarson <sarsonmedia@gmail.com>
2017-05-16 15:19:27 -0400

    add checkpoint-4

--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -3,7 +3,21 @@ require "bloc_works/version"
 module BlocWorks
   class Application
      def call(env)
-       [200, {'Content-Type' => 'text/html'}, ["Hello Blocheads!"]]
-     end
-   end
+       if env['PATH_INFO'] == '/favicon.ico'
+         return [404, {'Content-Type' => 'text/html'}, []]
+       end
+
+       klass, action = controller_and_action(env)
+       controller = klass.new(env)
+       text = controller.send(action)
+
+       if controller.has_response?
+         status, header, response = controller.get_response
+         [status, header, [response.body].flatten]
+       else
+         [200, {'Content-Type' => 'text/html'}, [text]]
+       end
+
+    end
+  end
 end
--- lib/bloc_works/controller.rb
+++ lib/bloc_works/controller.rb
@@ -5,11 +5,35 @@ module BlocWorks
    class Controller
      attr_accessor :request
 
-     def initialize(env)
-       @request = env
+     def request
+       @request ||= Rack::Request.new(@env)
      end
 
-     def render(view, locals = {})
+     def params
+       request.params
+     end
+
+     def response(text, status = 200, headers = {})
+        raise "Cannot respond multiple times" unless @response.nil?
+        @response = Rack::Response.new([text].flatten, status, headers)
+      end
+
+  #  #2
+      def render(*args)
+        response(create_reponse_array(*args))
+      end
+
+  #  #3
+      def get_response
+        @response
+      end
+
+  #  #4
+      def has_response?
+        !@response.nil?
+      end
+
+     def create_reponse_array(view, locals = {})
        filename = File.join("app", "views", controller_dir, "#{view}.html.erb")
        template = File.read(filename)
        eruby = Erubis::Eruby.new(template)
@@ -21,5 +45,6 @@ module BlocWorks
       klass.slice!("Controller")
       BlocWorks.snake_case(klass)
     end
-  end
+
+  end 
 end
